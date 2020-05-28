94d3c15320d5920aceb1eeda63e3cf6d97eb666f
linuxanselmo <programadorantonio@gmail.com>
2020-05-28 01:13:13 -0300

    atualizando o github

---
+++ database/migrations/2020_05_16_214129_create_anunciantes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnunciantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anunciantes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->boolean('is_anunciante')->default(false);
+            $table->boolean('ativo')->default(true);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anunciantes');
+    }
+}
